b92f3387b2914847fee7700bc9652568b3841085
YY <z3388638@gmail.com>
2018-08-12 19:38:24 +0800

    update meta

--- index.user.js
+++ index.user.js
@@ -7,6 +7,7 @@
 // @match        https://www.dcard.tw/*
 // @license      MIT
 // @homepage     https://github.com/x3388638/dcard-images
+// @updateURL    https://openuserjs.org/meta/x3388638/dcard-images.meta.js
 // @grant        none
 // ==/UserScript==
 
