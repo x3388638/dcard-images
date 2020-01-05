07e26dfd7c6aeed5f1bd828f5187bfa3a88623c5
YY <z3388638@gmail.com>
2020-01-06 02:02:01 +0800

    Fix BrowseBtn style

--- components/BrowseBtn.js
+++ components/BrowseBtn.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faImage } from '@fortawesome/free-solid-svg-icons'
 
 const BrowseBtn = () => {
-  return <div>123</div>
+  return (
+    <span
+      css={`
+        font-size: 18px;
+        background: rgb(196, 196, 196);
+        padding: 2px 5px;
+        color: #fff;
+        margin-left: 10px;
+        border-radius: 20px;
+        cursor: pointer;
+        &:hover {
+          background: #006aa6;
+        }
+      `}
+    >
+      <FontAwesomeIcon icon={faImage} />
+    </span>
+  )
 }
 
 export default BrowseBtn
