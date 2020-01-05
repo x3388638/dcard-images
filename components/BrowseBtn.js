8317aad8c674d111c93da8c6483a879b03d740bf
YY <z3388638@gmail.com>
2020-01-06 03:19:46 +0800

    Fetch images on clicking BrowseBtn

--- components/BrowseBtn.js
+++ components/BrowseBtn.js
@@ -2,21 +2,22 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImage } from '@fortawesome/free-solid-svg-icons'
 
-const BrowseBtn = () => {
+const BrowseBtn = ({ onClick }) => {
   return (
     <span
       css={`
         font-size: 18px;
         background: rgb(196, 196, 196);
-        padding: 2px 5px;
+        padding: 6px 8px;
         color: #fff;
         margin-left: 10px;
-        border-radius: 20px;
+        border-radius: 50%;
         cursor: pointer;
         &:hover {
           background: #006aa6;
         }
       `}
+      onClick={onClick}
     >
       <FontAwesomeIcon icon={faImage} />
     </span>
---
+++ components/Gallery.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Gallery = ({ images }) => {
+  return <span>{JSON.stringify(images)}</span>
+}
+
+export default Gallery
