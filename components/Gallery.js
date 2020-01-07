dd823a5fafbe326a5caf805e17225684c564533a
YY <yuying.chang@verizonmedia.com>
2020-01-07 17:57:40 +0800

    Add grid in Gallery to render images

--- components/Gallery.js
+++ components/Gallery.js
@@ -14,6 +14,9 @@ const Backdrop = styled.div`
   z-index: 20020;
   transition: opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
   opacity: 0;
+  padding: 0 calc(3% + 25px);
+  overflow: auto;
+  box-sizing: border-box;
 `
 
 const CloseBtn = styled.span`
@@ -23,7 +26,7 @@ const CloseBtn = styled.span`
   color: #ddd;
   font-size: 25px;
   cursor: pointer;
-  position: absolute;
+  position: fixed;
   top: 10px;
   right: 2%;
   line-height: 25px;
@@ -32,6 +35,47 @@ const CloseBtn = styled.span`
   &:hover {
     color: #fff;
     transform: rotate(360deg);
+  }
+`
+
+const ImageGridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 20px;
+  margin: 50px 0;
+
+  @media screen and (max-width: 1480px) {
+    & {
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    & {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    & {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+`
+
+const ImageItem = styled.div`
+  border: 1px solid #fff;
+  border-radius: 8px;
+  height: 175px;
+  cursor: pointer;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: ${({ img }) => `url(${img})`};
+  transition: box-shadow 0.2s;
+
+  &:hover {
+    box-shadow: 0 0 10px 0px #f3f3f3;
   }
 `
 
@@ -55,8 +99,14 @@ const Gallery = ({ isOpen = false, images = [], onClose }) => {
           <CloseBtn onClick={onClose}>
             <FontAwesomeIcon icon={faTimesCircle} />
           </CloseBtn>
-          {JSON.stringify(images)}
           {/* image grid, carousel */}
+          <ImageGridContainer>
+            {images.map((imageData, i) => (
+              <ImageItem key={i} img={imageData.img}>
+                {/* show floor, gender, name(school, dept), download btn, date, index */}
+              </ImageItem>
+            ))}
+          </ImageGridContainer>
         </Backdrop>
       )}
     </Transition>
